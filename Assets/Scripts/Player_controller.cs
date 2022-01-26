c73eddf5ff09f0e83a3af7c1ba79a1898479fb64
Aneesh Babu <73874288+Aneeshbabu2106@users.noreply.github.com>
2022-01-26 15:32:42 +0530

    renaming variables for better understanding

--- Assets/Scripts/Player_controller.cs
+++ Assets/Scripts/Player_controller.cs
@@ -5,17 +5,18 @@ public class Player_controller : MonoBehaviour
     public Animator animator;
     private Rigidbody2D playerRigidBody;
 
+    private float verticalInput;
+    private float horizontalInput;
+
     [SerializeField] private float speedForce = 0.0f;
     [SerializeField] private float jumpForce = 0.0f;
-
-    private float jump;
-    private float speed;
+    [SerializeField] private bool isGrounded = false;
 
     private bool isCrouching = false;
     private bool isStaffAttacking = false;
     private  bool isShooting = false;
     private bool isPushing = false;
-    [SerializeField] private bool isGrounded = false;
+    
     
     void Awake() {
         playerRigidBody = gameObject.GetComponent<Rigidbody2D>();
@@ -24,8 +25,8 @@ public class Player_controller : MonoBehaviour
     void Update()
     {       
         //player input
-        speed = Input.GetAxis("Horizontal");
-        jump = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
         isCrouching = Input.GetKey(KeyCode.LeftControl);
         isStaffAttacking = Input.GetKey(KeyCode.E);
         isShooting = Input.GetKey(KeyCode.Space);
@@ -49,11 +50,11 @@ public class Player_controller : MonoBehaviour
     {
         //player horizontal movement
         Vector3 position = transform.position;
-        position.x += speed * speedForce * Time.deltaTime;
+        position.x += horizontalInput * speedForce * Time.fixedDeltaTime;
         transform.position = position;
 
         //player vertical movement
-        if (jump > 0 && isGrounded)
+        if (verticalInput > 0 && isGrounded && !isPushing && !isStaffAttacking)
         {
             playerRigidBody.AddForce(new Vector2(0,jumpForce),ForceMode2D.Force);
             Debug.Log(jumpForce);
@@ -64,14 +65,14 @@ public class Player_controller : MonoBehaviour
     private void PlayAnimation ()
     { 
         //animating run, idle animation
-        animator.SetFloat("speed",Mathf.Abs(speed));
+        animator.SetFloat("speed",Mathf.Abs(horizontalInput));
 
         //flipping
         Vector3 scale = transform.localScale;
-        if (speed < 0)
+        if (horizontalInput < 0)
         {
             scale.x = -1f * Mathf.Abs(scale.x);
-        }else if(speed > 0)
+        }else if(horizontalInput > 0)
         {
             scale.x = Mathf.Abs(scale.x);
         }
@@ -118,7 +119,7 @@ public class Player_controller : MonoBehaviour
         }
 
         //jumping mechanics
-        if (jump > 0 && isGrounded)
+        if (verticalInput > 0 && isGrounded)
         {
             animator.SetBool("isJumping",true);  
         }
