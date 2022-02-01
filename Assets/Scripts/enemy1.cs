390c321befc6785e44feae48e6c7fc6b25933087
Aneesh Babu <73874288+Aneeshbabu2106@users.noreply.github.com>
2022-02-01 17:04:15 +0530

    enemies in all scenes

--- Assets/Scripts/enemy1.cs
+++ Assets/Scripts/enemy1.cs
@@ -10,24 +10,27 @@ public class enemy1 : MonoBehaviour
     [SerializeField] private bool isfacingLeft = true;
 
     private void Update() {
-        if (transform.position.x > leftBound && isfacingLeft)    //going left
+        if (transform.position.x > leftBound && isfacingLeft)          //going left
         {
             Vector3 position = transform.position;
             position.x -= moveSpeed * Time.deltaTime;
-            transform.position = position;
-        }else if (isfacingLeft)                                //flipping to right
+            transform.position = position; 
+        }
+        else if (isfacingLeft)                                        //flipping to right
         {
             isfacingLeft = false;
             Vector3 scale = transform.localScale;
             scale.x = Mathf.Abs(scale.x);
             transform.localScale=scale;
-        }else if (transform.position.x < rightBound && !isfacingLeft) //going right
+        }
+        else if (transform.position.x < rightBound && !isfacingLeft)  //going right
         {
             Vector3 position = transform.position;
             position.x += moveSpeed * Time.deltaTime;
             transform.position = position;
-        }else if (!isfacingLeft)     
-        {
+        }
+        else if (!isfacingLeft)                                       //flipping to left
+        { 
             isfacingLeft = true;
             Vector3 scale = transform.localScale;
             scale.x = -1f *Mathf.Abs(scale.x);
